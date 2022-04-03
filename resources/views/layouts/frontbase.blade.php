95ee8fd993e00f8d0c7d0710c0841ae7fc680d30
aeturkaslan <aeturkaslan@hotmail.com>
2022-04-03 22:56:25 +0300

    template implementation

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,20 +1,49 @@
-<html>
-    <head>
-        <title>App Name - @yield('title')</title>
-        @yield('head')
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Carbook - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>@yield("title")</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/animate.css">
+    
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/aos.css">
+
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/icomoon.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/style.css">
+
+    @yield("head")
+  </head>
+
+
     <body>
-        <h1>Header</h1>
+        @include("home.header")
 
         @section('sidebar')
-            This is the master sidebar.
+            
         @show
  
         <div class="container">
             @yield('content')
         </div>
 
-        <h1>Footer</h1>
+        @include("home.footer")
         @yield('foot')
     </body>
 </html>
