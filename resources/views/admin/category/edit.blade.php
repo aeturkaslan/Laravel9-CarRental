87c6a06f53271d2a5771bf350ebd95e0be2ff795
aeturkaslan <aeturkaslan@hotmail.com>
2022-04-22 21:38:59 +0300

    data updating page for admin panel

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.adminbase')
+ 
+@section('title', 'Edit Category: '.$data->title)
+ 
+@section('content')
+        <!--PAGE CONTENT -->
+        <div id="content">
+
+            <div class="inner" style="min-height:1200px;">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <h2>Edit {{ $data->title }}</h2>
+
+                    </div>
+                </div>
+
+                
+
+                <hr />
+
+
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    Category Elements
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <form role="form" action="/admin/category/update/{{ $data->id }}" method="post">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <label>Title</label>
+                                                    <input type="text" class="form-control" name='title' value="{{ $data->title }}">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Keywords</label>
+                                                    <input type="text" class="form-control" name='keywords' value="{{ $data->keywords }}">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Description</label>
+                                                    <input type="text" class="form-control" name='description' value="{{ $data->description }}">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Image</label>
+                                                    <input type="file" />
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Status</label>
+                                                    <select class="form-control" name="status">
+                                                        <option selected>{{ $data->status }}</option>
+                                                        <option>True</option>
+                                                        <option>False</option>
+                                                    </select>
+                                                </div>
+
+                                                <button type="submit" class="btn btn-primary">Update Data</button>
+
+
+                                            </form>
+
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                            
+
+
+
+
+
+
+
+
+            </div>
+
+            
+
+
+
+
+        </div>
+       <!--END PAGE CONTENT -->
+@endsection
